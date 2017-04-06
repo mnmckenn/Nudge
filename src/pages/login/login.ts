19e886df50f77a14567e7e4c3f57ebb5074c15fc
Kenneth Neaville <neaville@uw.edu>
2017-04-06 10:52:01 -0700

    Adding basic login screen and connection to internal app

---
+++ src/pages/login/login.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { MyApp } from '../../app/app.component';
+import { NavController } from 'ionic-angular';
+import { TabsPage } from '../tabs/tabs';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'login.html',
+  controller: 'LoginController'
+})
+export class LoginPage {
+
+  constructor(public navCtrl: NavController	) {
+      
+  }
+
+  login() {
+      this.navCtrl.push(TabsPage);
+  }
+
+}
