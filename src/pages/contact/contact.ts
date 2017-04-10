a67c8b0edd199180e8753b85fe08d739fc0b66d5
Kenneth Neaville <neaville@uw.edu>
2017-04-10 10:33:31 -0700

    Added logout button to settings page

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+
 
 @Component({
   selector: 'page-contact',
@@ -18,6 +20,10 @@ export class ContactPage {
 	 sheet.innerHTML = ".fixed-content {background-color: rgb(241, 185, 51);";
 	 document.body.appendChild(sheet);
 	 
+  }
+
+  logout() {
+      document.location.href = 'index.html';
   }
 
 }
