5c1ce9a9886282e7e6bca30df847212489d8256c
Kenneth Neaville <neaville@uw.edu>
2017-04-11 11:43:29 -0700

    Added Google maps code, API key, node modules, etc.

--- src/pages/ask/ask.ts
+++ src/pages/ask/ask.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {googlemaps} from 'googlemaps';
 
 
 @Component({
@@ -18,6 +19,16 @@ export class AskPage {
 	 sheet.innerHTML = ".fixed-content {background-color: rgb(188, 102, 255);}";
 	 document.body.appendChild(sheet);
 
+     var seattle = {lat: 47.606, lng: -122.3321};
+     console.log(seattle);
+     var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 14,
+      center: seattle
+     });
+     var marker = new google.maps.Marker({
+      position: seattle,
+      map: map
+     });
   }
 
 }
