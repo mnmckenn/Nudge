ab6a5d9f3dbcf6374cca98720a8d70ad20c82dfc
Kenneth Neaville <neaville@uw.edu>
2017-04-17 20:52:02 -0700

    Able to retrieve date with TypeScript (moved from internally stored to external, must allow CORs until Nudge is actually deployed)

--- src/pages/categories/categories.ts
+++ src/pages/categories/categories.ts
@@ -1,18 +1,37 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
+// import { NudgeData } from '../../assets/data/nudges.json';
+import {HttpProvider} from '../../providers/http-provider';
 
 @Component({
   selector: 'page-home',
+  providers: [HttpProvider],
   templateUrl: 'categories.html'
 })
 export class CategoryPage {
 
-  constructor(public navCtrl: NavController) {
-  	
+  newsData: any;
+
+  constructor(public navCtrl: NavController, public httpProvider:HttpProvider) {
+  	this.getdata();
+  }
+
+  getdata(){
+  this.httpProvider.getJsonData().subscribe(
+    result => {
+      this.newsData=result;
+      console.log("Success : "+this.newsData);
+    },
+    err =>{
+      console.error("Error : "+err);
+    } ,
+    () => {
+      console.log('getData completed');
+    }
+   );
   }
 
   ionViewWillEnter() {
   	
   }
-
 }
